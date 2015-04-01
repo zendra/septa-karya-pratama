ee5c05973077d6775555edc3442223300eb3d8fd
Afid <afideri@gmail.com>
2015-04-01 09:29:39 +0700

    remove class="item"

--- application/views/home/v_carousel.php
+++ application/views/home/v_carousel.php
@@ -1,9 +1,9 @@
 <section id="main-slider">
     <div id="owl" class="owl-carousel">
-        <div class="item">
+        <div>
             <img src="<?php echo base_url() . 'assets/images/carousel/1-logo.png'; ?>" alt="Septa Karya Pratama">
         </div><!--/.item-->
-        <div class="item">
+        <div>
             <img src="<?php echo base_url() . 'assets/images/carousel/2-riverside.png'; ?>" alt="Riverside">
         </div><!--/.item-->        
     </div><!--/.owl-carousel-->
